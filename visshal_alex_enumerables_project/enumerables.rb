d03548cd1db0454dd72a0123ab675adeed24dd4f
Visshal Suresh <skvisshal@gmail.com>
2021-10-18 14:36:12 -0400

    all enumerable methods

--- visshal_alex_enumerables_project/enumerables.rb
+++ visshal_alex_enumerables_project/enumerables.rb
@@ -9,4 +9,40 @@ class Array
         self
     end
 
+    def my_select(&blk)
+        result = []
+        self.my_each do |ele|
+            result << ele if blk.call(ele)
+        end 
+        result
+    end
+
+    def my_reject(&blk)
+        result = []
+        self.my_each do |ele|
+            result << ele if !blk.call(ele)
+        end 
+        result
+    end
+
+    def my_any?(&blk)
+        self.my_each do |ele|
+            return true if blk.call(ele)
+        end
+        false
+    end
+
+    def my_all?(&blk)
+        self.my_each do |ele|
+            return false if !blk.call(ele)
+        end
+        true
+    end
+
 end
+
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
